5cffeddf749953bf838c9306aaec59a0850846b5
Robert Auer <robert.auer@cloudogu.com>
2017-12-12 09:52:36 +0100

    Catch case of empty CURRIP; #1

--- resources/usr/local/bin/ipchange.sh
+++ resources/usr/local/bin/ipchange.sh
@@ -9,19 +9,19 @@ IP_HAS_CHANGED=false
 
 function valid_ip()
 {
-    local  ip=$1
-    local  stat=1
+  local  ip=$1
+  local  stat=1
 
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        OIFS=$IFS
-        IFS='.'
-        ip=($ip)
-        IFS=$OIFS
-        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
-            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-        stat=$?
-    fi
-    return $stat
+  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+      OIFS=$IFS
+      IFS='.'
+      ip=($ip)
+      IFS=$OIFS
+      [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+          && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+      stat=$?
+  fi
+  return $stat
 }
 
 function checkIPChange(){
@@ -43,10 +43,10 @@ function checkIPChange(){
   # Check if system has got a new IP after reboot
   # or last IP was empty or not an IP
   if [ "${LASTIP}" != "${CURRIP}" ] && [ ! -z $LASTIP ] && valid_ip ${LASTIP}; then
-    IP_HAS_CHANGED=true
     echo "$(date +%T): IP has changed from >${LASTIP}< to >${CURRIP}<"
     # IP changed
-    if valid_ip ${CURRIP} ; then
+    if [ ! -z ${CURRIP} ] && valid_ip ${CURRIP} ; then
+      IP_HAS_CHANGED=true
       echo "$(date +%T): ${CURRIP} is a valid IP; setting fqdn"
       etcdctl --peers //"$(cat /etc/ces/node_master)":4001 set "/config/_global/fqdn" "${CURRIP}"
       ETCDCTL_EXIT=$?
@@ -57,29 +57,29 @@ function checkIPChange(){
         ETCDCTL_EXIT=$?
         sleep 0.25
       done
+      # Reinstall certificates if self-signed
+      CERT_TYPE=$(etcdctl --peers //"$(cat /etc/ces/node_master)":4001 get /config/_global/certificate/type)
+      if [ "$CERT_TYPE" == "selfsigned" ]; then
+        echo "$(date +%T): certificate type is selfsigned"
+        source /etc/environment;
+        if [ "$(cat /etc/ces/type)" == "vagrant" ]; then
+          end=$((SECONDS+20)) # wait for max. 20 seconds
+          while [ ! -f /usr/local/bin/ssl.sh ] && [ $SECONDS -lt $end ]
+          do
+            sleep 0.25
+            echo "$(date +%T): waiting for /usr/local/bin/ssl.sh to become available..."
+          done
+        fi
+        if [ -f /usr/local/bin/ssl.sh ]; then
+          /usr/local/bin/ssl.sh
+        else
+          echo "$(date +%T): /usr/local/bin/ssl.sh does not exist"
+        fi
+      else
+        echo "$(date +%T): certificate type is not selfsigned"
+      fi
     else
       echo "$(date +%T): ${CURRIP} is no valid IP!"
-    fi
-    # Reinstall certificates if self-signed
-    CERT_TYPE=$(etcdctl --peers //"$(cat /etc/ces/node_master)":4001 get /config/_global/certificate/type)
-    if [ "$CERT_TYPE" == "selfsigned" ]; then
-      echo "$(date +%T): certificate type is selfsigned"
-      source /etc/environment;
-      if [ "$(cat /etc/ces/type)" == "vagrant" ]; then
-        end=$((SECONDS+20)) # wait for max. 20 seconds
-        while [ ! -f /usr/local/bin/ssl.sh ] && [ $SECONDS -lt $end ]
-        do
-          sleep 0.25
-          echo "$(date +%T): waiting for /usr/local/bin/ssl.sh to become available..."
-        done
-      fi
-      if [ -f /usr/local/bin/ssl.sh ]; then
-        /usr/local/bin/ssl.sh
-      else
-        echo "$(date +%T): /usr/local/bin/ssl.sh does not exist"
-      fi
-    else
-      echo "$(date +%T): certificate type is not selfsigned"
     fi
   else
     echo "$(date +%T): IP has not changed or last IP (${LASTIP}) is empty or not an IP. Current IP = $CURRIP"
