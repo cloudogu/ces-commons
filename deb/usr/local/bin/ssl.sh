0c48e4d455f5cfc80bfb659f9aca35f2d695784b
Florian Gill <florian.gill@cloudogu.com>
2021-02-23 16:31:55 +0100

    Self-signed certificate is added to certification authority (#19)

--- deb/usr/local/bin/ssl.sh
+++ deb/usr/local/bin/ssl.sh
@@ -88,5 +88,8 @@ etcdctl --peers "//$(cat /etc/ces/node_master):4001" set /config/_global/certifi
 cat "${CERTIFICATE}" | etcdctl --peers "//$(cat /etc/ces/node_master):4001" set /config/_global/certificate/server.crt > /dev/null
 cat "${KEY}" | etcdctl --peers "//$(cat /etc/ces/node_master):4001" set /config/_global/certificate/server.key > /dev/null
 
+echo "adding ces self signed certificate to certificate authority"
+etcdctl get /config/_global/certificate/server.crt > /etc/ssl/certs/ces-self-signed.crt
+
 echo "removing temporary files"
 rm -rf "${SSL_DIR}"
