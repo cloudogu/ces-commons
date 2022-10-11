7f7549a595a7e724c801605b7f95f6bb326d053c
Simon Klein <simon.klein@cloudogu.com>
2022-10-11 13:54:13 +0200

    escape ipv6 url

--- deb/usr/local/bin/ssl_ces.sh
+++ deb/usr/local/bin/ssl_ces.sh
@@ -5,9 +5,9 @@ set -eo pipefail
 source /usr/local/bin/ssl.sh 365
 
 echo "writing certificate to etcd"
-etcdctl --peers "//$(cat /etc/ces/node_master):4001" set /config/_global/certificate/type selfsigned > /dev/null
-cat "${CERTIFICATE}" | etcdctl --peers "//$(cat /etc/ces/node_master):4001" set /config/_global/certificate/server.crt > /dev/null
-cat "${KEY}" | etcdctl --peers "//$(cat /etc/ces/node_master):4001" set /config/_global/certificate/server.key > /dev/null
+etcdctl --peers "//[$(cat /etc/ces/node_master)]:4001" set /config/_global/certificate/type selfsigned > /dev/null
+cat "${CERTIFICATE}" | etcdctl --peers "//[$(cat /etc/ces/node_master)]:4001" set /config/_global/certificate/server.crt > /dev/null
+cat "${KEY}" | etcdctl --peers "//[$(cat /etc/ces/node_master)]:4001" set /config/_global/certificate/server.key > /dev/null
 
 echo "adding ces self signed certificate to certificate authority"
 etcdctl get /config/_global/certificate/server.crt > /etc/ssl/certs/ces-self-signed.crt
